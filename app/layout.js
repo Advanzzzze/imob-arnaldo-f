8b7f6a3d63806a416b5c0ef87c2c82d1d6ccba37
Lymei <lymeicontato@gmail.com>
2023-05-23 10:37:10 -0300

    feat: added hero

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang='en'>
-			<body className={`bg-primary-color ${inter.className}`}>{children}</body>
+			<body className={`bg-primary-color-300 text-white ${inter.className}`}>{children}</body>
 		</html>
 	);
 }
--- app/page.js
+++ app/page.js
@@ -3,7 +3,9 @@ import Hero from './components/Hero';
 export default function Home() {
 	return (
 		<main>
-			<div></div>
+			<div>
+				<Hero />
+			</div>
 		</main>
 	);
 }
