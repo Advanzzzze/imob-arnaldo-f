2111028c5dc4649e8304c45774f31bedad1fcb01
Lymei <lymeicontato@gmail.com>
2023-05-23 08:58:59 -0300

    feat: added colors

--- app/page.js
+++ app/page.js
@@ -1,7 +1,11 @@
+import Hero from './components/Hero';
+
 export default function Home() {
 	return (
 		<main>
-			<div></div>
+			<div>
+				<Hero />
+			</div>
 		</main>
 	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+			colors: {
+				'primary-color-300': '#03024E',
+				'primary-color-300': '#05048A',
+				'primary-color-300': '#0805C5',
+				'secondary-color-300': '#1C78D4',
+				'secondary-color-300': '#549ee8',
+				'secondary-color-300': '#90c0f0',
+				'ascent-color-300': '#2BDA1F',
+				'ascent-color-300': '#61e758',
+				'ascent-color-300': '#9ff199',
+			},
+		},
+	},
+	plugins: [],
+};
