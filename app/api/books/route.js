6ab2d9e4d63970db171efa79cce0830ec803e4d9
Lymei <lymeicontato@gmail.com>
2023-05-31 17:33:18 -0300

    feat: finalized houses api

--- app/api/books/route.js
+++ app/api/books/route.js
@@ -1,8 +1,8 @@
 import { drizzle } from 'drizzle-orm/planetscale-serverless'
 import { connect } from '@planetscale/database'
 import { NextResponse } from 'next/server'
-import { houses } from '@/db/schema'
 import { eq } from 'drizzle-orm'
+import { books } from '@/db/schema'
 
 // create the connection
 const connection = connect({
@@ -14,7 +14,7 @@ const connection = connect({
 const db = drizzle(connection)
 
 export async function GET() {
-    const result = await db.select().from(houses)
+    const result = await db.select().from(books)
 
     return NextResponse.json({ response: result })
 }
@@ -22,37 +22,9 @@ export async function GET() {
 export async function POST(request) {
     const body = await request.json()
 
-    const date = new Date()
+    const newBook = {}
 
-    const today = `${date.getFullYear()}-${
-        date.getMonth() + 1
-    }-${date.getDate()}`
-
-    const time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
-
-    const dateTime = `${today}T${time}`
-
-    const newHouse = {
-        name: body.name,
-        data: {
-            isHouse: body.isHouse ? body.isHouse : false,
-            address: body.address ? body.address : '',
-            info: {
-                area: body.area ? body.area : '',
-                status: body.status ? body.status : '',
-                bedrooms: body.bedrooms ? body.bedrooms : '',
-                suites: body.suites ? body.suites : '',
-                restrooms: body.restrooms ? body.restrooms : '',
-                cars: body.cars ? body.cars : '',
-            },
-            description: body.description ? body.description : '',
-            tags: body.tags ? body.tags : [], // {title: '', iconId: 0}
-            images: body.images ? body.images : [],
-        },
-        createdAt: dateTime,
-    }
-
-    const result = await db.insert(houses).values(newHouse)
+    const result = await db.insert(books).values(newHouse)
 
     return NextResponse.json({ response: newHouse })
 }
@@ -60,7 +32,7 @@ export async function POST(request) {
 export async function PUT(request) {
     const body = await request.json()
 
-    const response = await db.delete(houses).where(eq(houses.id, body.id))
+    const response = await db.delete(books).where(eq(books.id, body.id))
 
     return NextResponse.json({ response: response })
 }
@@ -90,9 +62,9 @@ export async function PATCH(request) {
     }
 
     const response = await db
-        .update(houses)
+        .update(books)
         .set(newHouse)
-        .where(eq(houses.id, body.id))
+        .where(eq(books.id, body.id))
 
     return NextResponse.json({ response: response })
 }
