95b08392fe3a8811ed6b061c4993d683e23188eb
Kevyn Lopes <kevynlopes2019@gmail.com>
2024-04-24 20:53:59 -0300

    mais problema

--- app/utils/consts.js
+++ app/utils/consts.js
@@ -1,10 +1,7 @@
 export const houses = [
     {
         id: '0',
-<<<<<<< HEAD
-=======
         name: 'Casa',
->>>>>>> b771b256e20c7f4270e405ca26e2f46c5c2a712d
         data: {
             isHouse: true,
             location: 'Florea Bosque Da Saúde',
@@ -16,22 +13,15 @@ export const houses = [
                 size: '20m',
                 status: 'Pronto',
             },
-<<<<<<< HEAD
-            images: ['/images/imovel-de-luxo.jpg'],
-=======
             images: [
                 '/images/imovel-de-luxo.jpg',
                 '/images/imovel-de-luxo.jpg',
             ],
->>>>>>> b771b256e20c7f4270e405ca26e2f46c5c2a712d
         },
     },
     {
         id: '1',
-<<<<<<< HEAD
-=======
         name: 'Casa',
->>>>>>> b771b256e20c7f4270e405ca26e2f46c5c2a712d
         data: {
             isHouse: true,
             location: 'Florea Bosque Da Saúde',
@@ -48,10 +38,7 @@ export const houses = [
     },
     {
         id: '2',
-<<<<<<< HEAD
-=======
         name: 'Casa',
->>>>>>> b771b256e20c7f4270e405ca26e2f46c5c2a712d
         data: {
             isHouse: true,
             location: 'Florea Bosque Da Saúde',
@@ -68,10 +55,7 @@ export const houses = [
     },
     {
         id: '3',
-<<<<<<< HEAD
-=======
         name: 'Casa',
->>>>>>> b771b256e20c7f4270e405ca26e2f46c5c2a712d
         data: {
             isHouse: true,
             location: 'Florea Bosque Da Saúde',
@@ -89,9 +73,6 @@ export const houses = [
                 '/images/imovel-de-luxo.jpg',
                 '/images/imovel-de-luxo.jpg',
                 '/images/imovel-de-luxo.jpg',
-<<<<<<< HEAD
-                '/images/imovel-de-luxo.jpg',
-=======
             ],
         },
     },
@@ -110,7 +91,6 @@ export const houses = [
                 status: 'Pronto',
             },
             images: [
->>>>>>> b771b256e20c7f4270e405ca26e2f46c5c2a712d
                 '/images/imovel-de-luxo.jpg',
                 '/images/imovel-de-luxo.jpg',
                 '/images/imovel-de-luxo.jpg',
@@ -118,28 +98,8 @@ export const houses = [
         },
     },
     {
-<<<<<<< HEAD
-        id: '4',
-        data: {
-            isHouse: true,
-            location: 'Florea Bosque Da Saúde',
-            address: 'Rua Marcos Fernandes, 156',
-            city: 'Saúde, São Paulo',
-            description:
-                'More em um projeto de torres independentes criadas a partir de um Design Biofílico que chegou para cria...',
-            info: {
-                size: '20m',
-                status: 'Pronto',
-            },
-            images: ['/images/imovel-de-luxo.jpg'],
-        },
-    },
-    {
-        id: '5',
-=======
         id: '5',
         name: 'Casa',
->>>>>>> b771b256e20c7f4270e405ca26e2f46c5c2a712d
         data: {
             isHouse: true,
             location: 'Florea Bosque Da Saúde',
@@ -151,22 +111,15 @@ export const houses = [
                 size: '20m',
                 status: 'Pronto',
             },
-<<<<<<< HEAD
-            images: ['/images/imovel-de-luxo.jpg'],
-=======
             images: [
                 '/images/imovel-de-luxo.jpg',
                 '/images/imovel-de-luxo.jpg',
             ],
->>>>>>> b771b256e20c7f4270e405ca26e2f46c5c2a712d
         },
     },
     {
         id: '6',
-<<<<<<< HEAD
-=======
         name: 'Casa',
->>>>>>> b771b256e20c7f4270e405ca26e2f46c5c2a712d
         data: {
             isHouse: true,
             location: 'Florea Bosque Da Saúde',
@@ -183,10 +136,7 @@ export const houses = [
     },
     {
         id: '7',
-<<<<<<< HEAD
-=======
         name: 'Casa',
->>>>>>> b771b256e20c7f4270e405ca26e2f46c5c2a712d
         data: {
             isHouse: true,
             location: 'Florea Bosque Da Saúde',
@@ -203,10 +153,7 @@ export const houses = [
     },
     {
         id: '8',
-<<<<<<< HEAD
-=======
         name: 'Casa',
->>>>>>> b771b256e20c7f4270e405ca26e2f46c5c2a712d
         data: {
             isHouse: true,
             location: 'Florea Bosque Da Saúde',
@@ -223,10 +170,7 @@ export const houses = [
     },
     {
         id: '9',
-<<<<<<< HEAD
-=======
         name: 'Casa',
->>>>>>> b771b256e20c7f4270e405ca26e2f46c5c2a712d
         data: {
             isHouse: true,
             location: 'Florea Bosque Da Saúde',
